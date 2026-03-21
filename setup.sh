8ac4450b3a67b7b2489a20d4542f1ba0351f7f61
mxve <git@mxve.de>
2026-03-21 15:16:09 +0100

    a!

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# ---------------------------------------------------- #
+
 apt-get update
 apt-get upgrade -y
 apt-get install -y gnupg build-essential sudo htop nload screen nano debian-goodies unzip zip curl git fail2ban rsync cifs-utils age zsh zsh-autosuggestions
 
-curl -fsSL https://raw.githubusercontent.com/mxve/server/main/.zshrc > /etc/zsh/zshrc
+# ---------------------------------------------------- #
 
 curl -fsSL https://raw.githubusercontent.com/mxve/server/main/sshd_config > /etc/ssh/sshd_config
 
@@ -18,22 +20,25 @@ done
 curl -fsSL https://raw.githubusercontent.com/mxve/server/main/issue.net/rotate.sh > /opt/issue.net/rotate.sh
 chmod +x /opt/issue.net/rotate.sh
 echo "bash /opt/issue.net/rotate.sh" >> /etc/ssh/sshrc
+echo "" > /etc/motd
 
 systemctl restart sshd
 
-echo "" > /etc/motd
+# ---------------------------------------------------- #
 
-CUR_TIMEZONE=$(cat /etc/timezone)
-if [ "$CUR_TIMEZONE" != "Europe/Berlin" ]; then
-    echo "Europe/Berlin" > /etc/timezone
-    unlink /etc/localtime
-    dpkg-reconfigure -f noninteractive tzdata
-fi
+timedatectl set-timezone "Europe/Berlin"
+
+# ---------------------------------------------------- #
 
 if [ ! -f ~/.ssh/id_ed25519 ]; then
     ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -q -N ""
 fi
 
-chsh -s /bin/zsh
 
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPC3xKgDpl5+3wEpqCGoJssyZHB9VI9/nqJNQhm9MVMd" >> /root/.ssh/authorized_keys
+
+# ---------------------------------------------------- #
+
+curl -fsSL https://raw.githubusercontent.com/mxve/server/main/.zshrc > /etc/zsh/zshrc
+chsh -s /bin/zsh
+zsh && exit
